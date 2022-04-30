195299cde4fbd33b482c4e2da9836633adc39aa4
Rafael705 <pontesr502@gmail.com>
2022-04-30 09:47:40 -0300

    testando

--- _3 Semestre/POO/Ex04/Program.java
+++ _3 Semestre/POO/Ex04/Program.java
@@ -4,6 +4,8 @@ public class Program
 {
     public static void main(String[] args) 
     {
+        System.out.println("testando");
+        
         Circuferencia circuferencia1; 
         Retangulo retangulo1; 
         

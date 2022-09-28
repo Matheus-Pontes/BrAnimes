8d93b920a792673583007bf89ef33820b43ec836
Matheus-Pontes <pontesm10@outlook.com>
2022-09-28 20:53:27 -0300

    Mensagem para o usuário. Erro de servidor. E estrutura de valores iniciais.

--- _4 Semestre/BancoDeDados/AnaliseTimes/frontend/script.js
+++ _4 Semestre/BancoDeDados/AnaliseTimes/frontend/script.js
@@ -1,4 +1,11 @@
 const $estados = document.querySelector('#estados');
+const $grafBR = document.querySelector('#brasileirao');
+const $grafLB = document.querySelector('#libertadores');
+const $grafCBR = document.querySelector('#mundial');
+const $grafMD = document.querySelector('#copaDoBrasil');
+const $grafSULA = document.querySelector('#sulamericana');
+const $message = document.querySelector('.message');
+const estadoSP = 25; 
 
 google.charts.load('current', { 'packages': ['corechart'] });
 google.charts.setOnLoadCallback(drawChartBR);
@@ -6,6 +13,11 @@ google.charts.setOnLoadCallback(drawChartLiberta);
 google.charts.setOnLoadCallback(drawChartMundial);
 google.charts.setOnLoadCallback(drawChartCopaDoBrasil);
 google.charts.setOnLoadCallback(drawChartSulamericana);
+
+function valoresIniciais() {
+    $message.style.display = 'none';
+    $estados.value = estadoSP;
+}
 
 async function drawChartBR() {
     const times = await GetBrasileirao().then(data => data);
@@ -26,7 +38,7 @@ async function drawChartBR() {
         is3D: true
     };
 
-    let chart = new google.visualization.PieChart(document.getElementById('brasileirao'));
+    let chart = new google.visualization.PieChart($grafBR);
 
     chart.draw(data, options);
 }
@@ -50,7 +62,7 @@ async function drawChartLiberta() {
         is3D: true
     };
 
-    let chart = new google.visualization.PieChart(document.getElementById('libertadores'));
+    let chart = new google.visualization.PieChart($grafLB);
 
     chart.draw(data, options);
 }
@@ -74,7 +86,7 @@ async function drawChartMundial() {
         is3D: true
     };
 
-    let chart = new google.visualization.PieChart(document.getElementById('mundial'));
+    let chart = new google.visualization.PieChart($grafMD);
 
     chart.draw(data, options);
 }
@@ -98,7 +110,7 @@ async function drawChartCopaDoBrasil() {
         is3D: true
     };
 
-    let chart = new google.visualization.PieChart(document.getElementById('copaDoBrasil'));
+    let chart = new google.visualization.PieChart($grafCBR);
 
     chart.draw(data, options);
 }
@@ -122,7 +134,7 @@ async function drawChartSulamericana() {
         is3D: true
     };
 
-    let chart = new google.visualization.PieChart(document.getElementById('sulamericana'));
+    let chart = new google.visualization.PieChart($grafSULA);
 
     chart.draw(data, options);
 }
@@ -136,43 +148,67 @@ $estados.addEventListener('change', function() {
 });
 
 async function GetBrasileirao() {
-    const [ times ] = await fetch(`http://localhost:3000/brasileirao/${document.getElementById('estados').value}`).then(data => data.json());
+    const [ times ] = await fetch(`http://localhost:3000/brasileirao/${$estados.value}`).then(data => data.json());
 
     return times;
 }
 
 async function GetLibertadores() {
-    const [ times ] = await fetch(`http://localhost:3000/libertadores/${document.getElementById('estados').value}`).then(data => data.json());
+    const [ times ] = await fetch(`http://localhost:3000/libertadores/${$estados.value}`).then(data => data.json());
 
     return times;
 }
 
 async function GetMundial() {
-    const [ times ] = await fetch(`http://localhost:3000/mundial/${document.getElementById('estados').value}`).then(data => data.json());
+    const [ times ] = await fetch(`http://localhost:3000/mundial/${$estados.value}`).then(data => data.json());
 
     return times;
 }
 
 async function GetCopaDoBrasil() {
-    const [ times ] = await fetch(`http://localhost:3000/copadobrasil/${document.getElementById('estados').value}`).then(data => data.json());
+    const [ times ] = await fetch(`http://localhost:3000/copadobrasil/${$estados.value}`).then(data => data.json());
 
     return times;
 }
 
 async function GetSulamericana() {
-    const [ times ] = await fetch(`http://localhost:3000/sulamericana/${document.getElementById('estados').value}`).then(data => data.json());
+    const [ times ] = await fetch(`http://localhost:3000/sulamericana/${$estados.value}`).then(data => data.json());
 
     return times;
 }
 
+function showGrafs(deuErro) {
+    if (deuErro) {
+        $message.style.display = 'block';
+        $grafBR.style.display = 'none';
+        $grafLB.style.display = 'none';
+        $grafMD.style.display = 'none';
+        $grafCBR.style.display = 'none';
+        $grafSULA.style.display = 'none';
+    }
+    else{
+        $message.style.display = 'none';
+        $grafBR.style.display = 'block';
+        $grafLB.style.display = 'block';
+        $grafMD.style.display = 'block';
+        $grafCBR.style.display = 'block';
+        $grafSULA.style.display = 'block';
+    }
+}
+
 async function GetEstados() {
-    const [ estados ] = await fetch('http://localhost:3000/estados').then(data => data.json());
+    const [ estados ] = await fetch('http://localhost:3000/estados').then(data => data.json()).catch(err => { 
+        showGrafs(err.message);
+    });
 
     estados.forEach(estado => {
         $estados.innerHTML += `
             <option value="${estado.EstadoId}">${estado.Descricao}</option>
         `;
-    })
+    });
+
+    
+    valoresIniciais();
 }
 
 GetEstados();
